c33e40bc87c8a41d8305347d1072e356cb80f9fd
Brent Allen <brenta@microsoft.com>
2019-12-19 23:46:11 -0800

    day 20 part 2

--- day/20/MazeTeleport/src/point.rs
+++ day/20/MazeTeleport/src/point.rs
@@ -164,6 +164,12 @@ impl Direction {
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
+pub enum PortalDirection {
+    In,
+    Out,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub enum MapTile {
     Open,
     Wall,
@@ -183,7 +189,7 @@ impl MapTile {
         match self {
             MapTile::Open => &"  ",
             MapTile::Wall => &"##",
-            MapTile::Portal(label, _) => &label, 
+            MapTile::Portal(label, _) => &label,
         }
     }
 
@@ -196,10 +202,13 @@ impl MapTile {
     }
 }
 
+type Coordinate = (Point, i32);
+
 #[derive(Clone)]
 pub struct Map {
     pub tiles: HashMap<Point, MapTile>,
     pub labeled: HashMap<String, Point>,
+    pub largest: Point,
 }
 
 impl Map {
@@ -207,6 +216,7 @@ impl Map {
         let mut s = Self {
             tiles: HashMap::new(),
             labeled: HashMap::new(),
+            largest: Point::new(0, 0),
         };
 
         let lines: Vec<&str> = chars.lines().collect();
@@ -216,6 +226,13 @@ impl Map {
             //let line = line.trim();
             for (x, c) in line.chars().enumerate() {
                 p = Point::new(x.try_into().unwrap(), y.try_into().unwrap());
+
+                if p.x > s.largest.x {
+                    s.largest.x = p.x;
+                } else if p.y > s.largest.y {
+                    s.largest.y = p.y;
+                }
+
                 if let Some(tile) = MapTile::from_ascii(c) {
                     if tile == MapTile::Open {
                         for direction in &DIRECTIONS {
@@ -260,38 +277,64 @@ impl Map {
         x.unwrap()
     }
 
-    pub fn neighbors(&self, point: &Point) -> Vec<(Point, &MapTile)> {
+    pub fn neighbors(&self, point: &Coordinate) -> Vec<(Coordinate, &MapTile)> {
         let mut v = Vec::new();
         for d in &DIRECTIONS {
-            let p2 = d.move_point(&point);
+            let p2 = d.move_point(&point.0);
             let tile = self.get_tile(&p2);
-            if let MapTile::Portal(label, point) = tile {
-                if let Some((otherpoint, otherlabel, ap)) = self.portals().iter().filter(|(p, l, ap)| *l == label && *p != p2).next() {
-                    v.push((**ap, self.get_tile(&otherpoint)));
-                }
-                else {
-                    println!("Unmatched label: {}", label);
+            if let MapTile::Portal(label, attach_point) = tile {
+                if let Some((inner, outer)) = self.portals().get(label) {
+                    if attach_point == *inner {
+                        v.push(((**outer, point.1 + 1), self.get_tile(&outer)));
+                    } else if attach_point == *outer {
+                        if point.1 > 0 {
+                            v.push(((**inner, point.1 - 1), self.get_tile(&inner)));
+                        }
+                    } else {
+                        println!("Portal: {}, attach_point: {:?}", label, attach_point);
+                        panic!("unexpected");
+                    }
+                } else {
+                    //println!("Unmatched label: {}", label);
                     assert!(label == "AA" || label == "ZZ");
                 }
-            }
-            else {
-                v.push((p2, tile));
+            } else {
+                v.push(((p2, point.1), tile));
             }
         }
         v
     }
 
-    pub fn portals(&self) -> Vec<(Point, &String, &Point)> {
-        self.tiles
-            .iter()
-            .filter_map(|(p, t)| match t {
-                MapTile::Portal(l, ap) => Some((*p, l, ap)),
-                _ => None,
-            })
-            .collect()
+    pub fn portals(&self) -> HashMap<&String, (&Point, &Point)> {
+        let mut list = HashMap::new();
+
+        for p in self.tiles.iter() {
+            if let (_, MapTile::Portal(label, attach_point)) = p {
+                if let Some(MapTile::Portal(_, other_attach_point)) = self
+                    .tiles
+                    .iter()
+                    .filter_map(|(p, t)| match t {
+                        MapTile::Portal(l, x) if l == label && x != attach_point => Some(t),
+                        _ => None,
+                    })
+                    .next()
+                {
+                    if attach_point.x < 5
+                        || attach_point.y < 5
+                        || attach_point.x > self.largest.x - 5
+                        || attach_point.y > self.largest.y - 5
+                    {
+                        list.insert(label, (other_attach_point, attach_point));
+                    } else {
+                        list.insert(label, (attach_point, other_attach_point));
+                    }
+                }
+            }
+        }
+        list
     }
 
-    pub fn traversable_neighbors(&self, point: &Point) -> Vec<(Point, &MapTile)> {
+    pub fn traversable_neighbors(&self, point: &Coordinate) -> Vec<(Coordinate, &MapTile)> {
         self.neighbors(point)
             .iter()
             .filter(|x| x.1.traversable())
@@ -299,20 +342,31 @@ impl Map {
             .collect()
     }
 
-    pub fn get_cost_map(&self, point: &Point) -> HashMap<Point, i32> {
+    pub fn get_cost_map(&self, point: &Coordinate, end: Option<&Coordinate>) -> HashMap<Coordinate, i32> {
         let mut cost_map = HashMap::new();
-        let mut search: Vec<(Point, i32)> = Vec::new();
+        let mut search: Vec<(Coordinate, i32)> = Vec::new();
         search.push((*point, 0));
         cost_map.insert(*point, 0);
+
+        let mut depth = 0;
 
         while search.len() > 0 {
             //println!("seachlen: {}", search.len());
             let (point, cost) = search.remove(0);
 
             for (p, t) in self.traversable_neighbors(&point) {
-                if *cost_map.get(&p).unwrap_or(&10000) > cost {
+                if cost_map.get(&p).map_or(true, |x| *x > cost) {
                     //println!("{},{} = {}   {}", p.x, p.y, cost, cost_map.len());
                     cost_map.insert(p, cost);
+                    if let Some(end) = end {
+                        if *end == p {
+                            return cost_map;
+                        }
+                    }
+                    if p.1 > depth {
+                        depth = p.1;
+                        println!("d: {}, {}", depth, cost);
+                    }
                     search.push((p, cost + 1));
                 }
             }
